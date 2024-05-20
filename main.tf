816c711530de1cc650cf8eb7dc9235abbb58d81f
ravi <raviteja9424@gmail.com>
2024-05-20 06:43:52 +0530

    sixth commit

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
-#Sample Terraform Script will be written heregit 
+#Sample Terraform Script will be written here
+
+
+resource resource "aws_instance" "MyEC2" {
+   ami             = ""
+   instance_type   = "t2.micro"
+}
