52e2d88f42609b80ec9c36340a9344e618baf243
ravi <raviteja9424@gmail.com>
2024-05-20 06:48:37 +0530

    seventh

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 #Sample Terraform Script will be written here
 
 
-resource resource "aws_instance" "MyEC2" {
+ resource "aws_instance" "MyEC2" {
    ami             = ""
    instance_type   = "t2.micro"
+
+ resource resource "aws_iam_user" "AWSIAM_USER" {
+    default = "ravi"
+ }
 }
